61d62ead7e5bdfd8a04f0a4ac97011e146607db6
igortky <igortky@gmail.com>
2021-02-18 17:22:12 -0300

    Create CALCULAAREA.php

---
+++ CALCULAAREA.php
@@ -0,0 +1,12 @@
+  function CALCULAAREA($Area){
+    
+   if ($Area == 1)
+    return 1;
+   else
+   return $Area**2 + ($Area-1)**2;
+       
+              
+        
+  }
+  
+ echo CALCULAAREA(3);
