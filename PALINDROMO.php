5806ddee827ce28e9ef9da0573d85116068d2c02
igortky <igortky@gmail.com>
2021-02-18 15:40:47 -0300

    Create PALINDROMO.php

---
+++ PALINDROMO.php
@@ -0,0 +1,16 @@
+<?php
+function PALINDROMO($STR){
+   
+      if($STR == strrev($STR) || strlen($STR)== 1) {
+              return false;
+            }
+      else {
+              return false;
+                              
+            }
+       
+              
+        
+  }
+  
+ echo PALINDROMO("a");
